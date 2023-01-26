b6162bf996d272e063b7b03903145c5278a42ba5
Shlomi Nugarker <shlomin1231@gmail.com>
2023-01-26 23:09:15 +0200

    added message.service and cmp

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,14 +5,17 @@ import { HEROES } from './mock-heroes';
 
 import { Observable, of } from 'rxjs';
 
+import { MessageService } from './message.service';
+
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../interfaces/hero';
-
-import { HEROES } from '../mock-heroes';
-
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,10 +12,14 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
