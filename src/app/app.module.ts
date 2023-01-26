296a82fef61b0dc14b71a22600490077aee007fa
Shlomi Nugarker <shlomin1231@gmail.com>
2023-01-26 21:30:13 +0200

    Display a list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [AppComponent, HeroesComponent],
